94ade2379c5488265acf3e68f63e3501263e17c2
Kumail Habib <kumailhabib12@gmail.com>
2021-10-21 23:34:07 +0500

    Change datetime format

--- config.def.h
+++ config.def.h
@@ -65,6 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "  %s",           "a %b %d %r" },
+	{ datetime, "  %s",           "%a %b %d %r" },
 	{wifi_essid, "   %s", "wlp1s0"},
 };
